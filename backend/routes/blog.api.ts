93fe796a58c0ed70965ada9398085e91eb4e5aac
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2024-05-29 13:59:24 +0900

    백앤드 분리

--- backend/routes/blog.api.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { blogController } from "../controllers/blog.controller";
-export const router = express.Router();
-
-router.post("/", blogController.createBlog);
-router.get("/", blogController.getBlog);
-router.put("/:id", blogController.updateBlog);
-router.delete("/:id", blogController.deleteBlog);
--- backend/routes/index.ts
+++
@@ -1,5 +0,0 @@
-import express from "express";
-import { router as blogApi } from "./blog.api";
-export const router = express.Router();
-
-router.use("/blog", blogApi);
